23309b38e84e3c8d337ddaf384e88b93e1613914
Kunal Kumar <61112064+kunalkumar37@users.noreply.github.com>
2022-03-29 08:05:51 +0530

    Add files via upload

---
+++ Bike.java
@@ -0,0 +1,10 @@
+
+    class Bike{
+        Bike(){
+            System.out.println("Bike is created");
+        }
+    
+    public static void main(String[] args) {
+        Bike b=new Bike();
+    }
+    }
